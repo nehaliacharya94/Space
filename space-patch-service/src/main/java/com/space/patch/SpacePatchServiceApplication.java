1a484116a220360658e49161935998dd7da68bea
Nehali Acharya <Nachary@eu.trimblecorp.net>
2018-02-20 11:52:11 +0530

    adding files

---
+++ space-patch-service/src/main/java/com/space/patch/SpacePatchServiceApplication.java
@@ -0,0 +1,12 @@
+package com.space.patch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacePatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacePatchServiceApplication.class, args);
+	}
+}
